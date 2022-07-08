045bd3e1ef80c074516ed9d196885d5d921df62d
Pedro <pedrocs378@gmail.com>
2022-07-08 00:10:08 -0300

    chore: ajusta react para 17.0.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { App } from './App'
 
 import { theme } from './styles/theme'
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider resetCSS theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
